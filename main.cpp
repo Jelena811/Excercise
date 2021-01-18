32792b027a95ada0ca0a2c342050de0c0423faae
JelenaT811 <tosic.jelena811@gmail.com>
2021-01-18 12:13:51 +0100

    local main adding change id

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@ int main()
 	std::cout<<Hello git<<std::endl;
 	std::cout<<Hello push once again<<std::endl;
 		std::cout<<Hello from local_dev!!!!<<std::endl;
+		std::cout<<Gerrit ID change!!!!<<std::endl;
 	return 0;
 }
